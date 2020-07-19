7490e23512ed756d062a9e508ee08a661a323927
tractors <huijianhua@163.com>
2020-07-20 04:48:58 +0800

    sql update

--- app/src/main/java/com/will/roombasic/Word.java
+++ app/src/main/java/com/will/roombasic/Word.java
@@ -13,11 +13,31 @@ public class Word {
     private String word;
     @ColumnInfo(name = "chinese_meaning")
     private String chineseMeaning;
+//    @ColumnInfo(name="foo_data")
+//    private boolean foo;
+//    @ColumnInfo(name = "bar_data")
+//    private boolean bar;
 
     public Word(String word, String chineseMeaning) {
         this.word = word;
         this.chineseMeaning = chineseMeaning;
     }
+
+//    public boolean isBar() {
+//        return bar;
+//    }
+//
+//    public void setBar(boolean bar) {
+//        this.bar = bar;
+//    }
+//
+//    public boolean isFoo() {
+//        return foo;
+//    }
+//
+//    public void setFoo(boolean foo) {
+//        this.foo = foo;
+//    }
 
     public void setId(int id) {
         this.id = id;
--- app/src/main/java/com/will/roombasic/WordDatabase.java
+++ app/src/main/java/com/will/roombasic/WordDatabase.java
@@ -2,11 +2,14 @@ package com.will.roombasic;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Word.class},version = 1,exportSchema = false)
+@Database(entities = {Word.class},version = 4,exportSchema = false)
 public abstract class WordDatabase extends RoomDatabase {
     public abstract WordDao getWordDao();
 
@@ -18,6 +21,9 @@ public abstract class WordDatabase extends RoomDatabase {
             synchronized (bytes){
                 if (null == INSTANCE) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),WordDatabase.class,"word_database")
+                            //.fallbackToDestructiveMigration()//强制性更新数据库，具有破坏性
+                            //.addMigrations(MIGRATION_2_3)
+                            .addMigrations(MIGRATION_3_4)
                             .build();
                 }
             }
@@ -25,5 +31,24 @@ public abstract class WordDatabase extends RoomDatabase {
 
         return INSTANCE;
     }
+
+    static final Migration MIGRATION_2_3 = new Migration(2,3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE word ADD COLUMN bar_data INTEGER NOT NULL DEFAULT 1");
+        }
+    };
+
+    static final Migration MIGRATION_3_4 = new Migration(3,4) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE word_temp (id INTEGER PRIMARY KEY NOT NULL ,english_word TEXT," +
+                    "chinese_meaning TEXT)");
+            database.execSQL("INSERT INTO word_temp (id,english_word,chinese_meaning)" +
+                    " SELECT id,english_word,chinese_meaning FROM word");
+            database.execSQL("DROP TABLE word");
+            database.execSQL("ALTER TABLE word_temp RENAME to word");
+        }
+    };
 
 }
